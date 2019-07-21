21f82ac034ec264edad710bc2859bd1729be31a4
Maxim <max_stupakov@mail.ru>
2019-07-21 18:47:51 +0300

    Update Version110Transformer.php

--- src/LiveControl/EloquentDataTable/VersionTransformers/Version110Transformer.php
+++ src/LiveControl/EloquentDataTable/VersionTransformers/Version110Transformer.php
@@ -10,40 +10,40 @@ class Version110Transformer implements VersionTransformerContract
 
     public function getSearchValue()
     {
-        if(isset($_POST['search']) && isset($_POST['search']['value']))
-            return $_POST['search']['value'];
+        if(isset($_GET['search']) && isset($_GET['search']['value']))
+            return $_GET['search']['value'];
         return '';
     }
 
     public function isColumnSearched($columnIndex)
     {
         return (
-            isset($_POST['columns'])
+            isset($_GET['columns'])
             &&
-            isset($_POST['columns'][$columnIndex])
+            isset($_GET['columns'][$columnIndex])
             &&
-            isset($_POST['columns'][$columnIndex]['search'])
+            isset($_GET['columns'][$columnIndex]['search'])
             &&
-            isset($_POST['columns'][$columnIndex]['search']['value'])
+            isset($_GET['columns'][$columnIndex]['search']['value'])
             &&
-            $_POST['columns'][$columnIndex]['search']['value'] != ''
+            $_GET['columns'][$columnIndex]['search']['value'] != ''
         );
     }
 
     public function getColumnSearchValue($columnIndex)
     {
-        return $_POST['columns'][$columnIndex]['search']['value'];
+        return $_GET['columns'][$columnIndex]['search']['value'];
     }
 
     public function isOrdered()
     {
-        return (isset($_POST['order']) && isset($_POST['order'][0]));
+        return (isset($_GET['order']) && isset($_GET['order'][0]));
     }
 
     public function getOrderedColumns()
     {
         $columns = [];
-        foreach($_POST['order'] as $i => $order)
+        foreach($_GET['order'] as $i => $order)
         {
             $columns[(int) $order['column']] = ($order['dir'] == 'asc' ? 'asc' : 'desc');
         }
